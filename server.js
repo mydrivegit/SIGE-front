50f576ed2427ce44f824480479b42e5ba94aec8f
alihasana <hasana.ali@gmail.com>
2018-06-02 15:00:44 +0200

    Heroku deployment changes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
 
-var app = express()
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000
+var app = express();
+app.use("/", serveStatic(path.join(__dirname, '/dist')));
+var port = process.env.PORT || 5000;
 
-app.listen(port)
-console.log('server started on port ' + port)
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+});
+
+app.listen(port);
+console.log('server started on port ' + port);
